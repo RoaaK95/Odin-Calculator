0004737ad29411686414f2b3925edb96dd914929
Roaa K <ruaakhuldoon.26@gmail.com>
2024-05-02 13:53:37 +0300

    IMPLEMENT CLEAR FUNCTION

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+class Calculator {
+ constructor(previousOperandTextElement,currentOperandTextElement)
+ {
+    this.previousOperandTextElement=previousOperandTextElement;
+    this.currentOperandTextElement=currentOperandTextElement;
+    this.clear();
+ }
+  
+ clear()
+ {
+    this.currentOperand = '' ;
+    this.previousOperand= '' ;
+    this.operation= undefined;
+ }
+}
 const numberButtons=document.querySelectorAll('[data-number]');
 const operationButtons=document.querySelectorAll('[data-operation]');
 const equalsButton=document.querySelector('[data-equals]');
@@ -5,3 +20,7 @@ const deleteButton=document.querySelector('[data-delete]');
 const clearButton=document.querySelector('[data-clear]');
 const previousOperandTextElement=document.querySelector('[data-previous-operand]');
 const currentOperandTextElement=document.querySelector('[data-current-operand]');
+
+const calculator=new Calculator(previousOperandTextElement,currentOperandTextElement);
+
+ 
